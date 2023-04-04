89c7541fb32f2080403f8ecf7907a66037ffe759
Frank Wall <fw@moov.de>
2023-04-04 10:56:01 +0200

    update PDK to 2.7.1

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 describe 'dovecot' do
-  context 'with default values for all parameters' do
-    it { should contain_class('dovecot') }
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts }
+
+      context 'with default parameters' do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('dovecot') }
+      end
+    end
   end
 end
